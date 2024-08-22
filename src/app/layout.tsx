0348050067c1b25563053ce4cbe40e60718ed7d3
Om Thorat <thoratom1104@gmail.com>
2024-08-22 15:36:53 +0530

    Setup mantine and root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
+"use client"
 import "@mantine/core/styles.css";
 
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider, Text } from "@mantine/core";
 import { NavbarMinimalColored } from "@/components/sidebar";
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
 
-export const metadata = {
-  title: "My Mantine app",
-  description: "I have followed setup instructions carefully",
-};
+// export const metadata = {
+//   title: "My Mantine app",
+//   description: "I have followed setup instructions carefully",
+// };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
     <html lang="en">
       <head>
@@ -22,12 +27,32 @@ export default function RootLayout({
       </head>
       <body>
         <MantineProvider>
-          <div className="flex">
-            <aside>
-              <NavbarMinimalColored />
-            </aside>
-            <main>{children}</main>
-          </div>
+        <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+          <Text fz={30} fw={700} ml="sm"> Audits. </Text>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
         </MantineProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <section id="search-bar" className="w-full min-h-screen">
-        <input type="text" className="p-2 rounded bg-slate-400" />
-      </section>
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
     </>
   );
 }
+
